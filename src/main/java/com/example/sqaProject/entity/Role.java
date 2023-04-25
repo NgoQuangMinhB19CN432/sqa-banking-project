75c1a471edd844621831378c2b04097db740ba44
mahado <minhnq.b19cn432.ptit.edu.vn>
2023-04-25 17:39:32 +0700

    thêm annotation set-get, sửa lại tên column để đúng với tên csdl, thêm biến status

--- src/main/java/com/example/sqaProject/entity/Role.java
+++ src/main/java/com/example/sqaProject/entity/Role.java
@@ -6,45 +6,34 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 @Table(name="role")
 public class Role {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name="code")
 	private int code;
 	
-	@Column(name="name", nullable = false)
+        @Column(name = "createdby")
+	private String createdBy;
+	@Column(name = "createddate")
+	private String createdDate;
+	@Column(name = "modifiedby")
+	private String modifiedBy;
+	@Column(name = "modifieddate")
+	private String modifiedDate;
+        
+	@Column(name="title", nullable = false)
 	private String name;
 	
 	@Column(name="content", nullable = false)
 	private String content;
 	
-	@Column(name="status", nullable = false)
+	@Column(name="role_status", nullable = false)
 	private String status;
-	public int getCode() {
-		return code;
-	}
-	public void setCode(int code) {
-		this.code = code;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public String getStatus() {
-		return status;
-	}
-	public void setStatus(String status) {
-		this.status = status;
-	}
-	
 }
