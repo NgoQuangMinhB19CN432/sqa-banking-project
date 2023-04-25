a2fad83003ab3751473a577d84506e7f8b1c7af3
mahado <minhnq.b19cn432.ptit.edu.vn>
2023-04-25 17:38:00 +0700

    thêm đối tượng admin vào csdl

--- src/main/java/com/example/sqaProject/entity/Admin.java
+++ src/main/java/com/example/sqaProject/entity/Admin.java
@@ -1,35 +1,34 @@
 package com.example.sqaProject.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name="admin")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Admin {
+    	@Id
+	@Column(name="id")
+	private int id;
+	@Column(name="username")
 	private String username;
+	@Column(name="password")
 	private String password;
+	@Column(name = "fullname")
 	private String fullname;
-	public Admin() {
-		// TODO Auto-generated constructor stub
-	}
-	public Admin(String username, String password, String fullname) {
-		super();
-		this.username = username;
-		this.password = password;
-		this.fullname = fullname;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getFullname() {
-		return fullname;
-	}
-	public void setFullname(String fullname) {
-		this.fullname = fullname;
-	}
-	
+	@Column(name="email")
+	private String email;
+	@Column(name="phone")
+	private String phone;
+	@Column(name = "address")
+	private String address;
 }
