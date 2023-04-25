1c90197a58de73022b86aa9640cd27211c8b2d3e
mahado <minhnq.b19cn432.ptit.edu.vn>
2023-04-25 17:42:07 +0700

    sửa lại tên hàm findByToDate

--- src/main/java/com/example/sqaProject/service/ISavingService.java
+++ src/main/java/com/example/sqaProject/service/ISavingService.java
@@ -6,6 +6,5 @@ import com.example.sqaProject.entity.Saving;
 
 public interface ISavingService {
 	List<Saving> findAll();
-	List<Saving> getBillByDate(String day1, String day2);
-	List<Saving> findAllByDate(String date);
+        List<Saving> findByToDate(String toDate);
 }
