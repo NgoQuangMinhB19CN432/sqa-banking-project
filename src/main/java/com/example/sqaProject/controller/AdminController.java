537b1f819169e2cc227e06d01ddbf1d6d18f92b8
mahado <minhnq.b19cn432.ptit.edu.vn>
2023-04-25 17:35:41 +0700

    làm lại /login từ set mặc định username-pass sang lấy data từ db

--- src/main/java/com/example/sqaProject/controller/AdminController.java
+++ src/main/java/com/example/sqaProject/controller/AdminController.java
@@ -1,6 +1,7 @@
 package com.example.sqaProject.controller;
 
 import com.example.sqaProject.entity.Admin;
+import com.example.sqaProject.service.impl.AdminService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +9,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Controller
 public class AdminController {
+	@Autowired
+	private AdminService adminService;
 	@GetMapping("/")
 	public String index(Model model) {
 		Admin admin= new Admin();
@@ -21,8 +25,13 @@ public class AdminController {
 	public String login(Admin admin,Model model, HttpSession session) {
 		String user=admin.getUsername();
 		session.setAttribute("name", user);
-		if(admin.getUsername().equals("admin") && admin.getPassword().equals("123456")) {
+		if(adminService.checkLogin(admin.getUsername(), admin.getPassword())) {
 			return "AdminPage";
+		}
+		else if(admin.getUsername().equals("") || admin.getPassword().equals("")) {
+			model.addAttribute("admin", admin);
+			model.addAttribute("message","please enter full username and password!!!");
+			return "login";
 		}
 		else {
 			model.addAttribute("admin", admin);
